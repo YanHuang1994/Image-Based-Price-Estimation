399c05d512167826fad0539ec023dc246bf79494
Yan Huang <yah423@lehigh.edu>
2024-02-18 12:43:11 -0500

    [mod]:"Crawling two types of guitars. Each guitar crawls 40 pages, with 60 items per page. There are a total of 2400 items for each type of guitar."

--- get_html.py
+++ get_html.py
@@ -4,28 +4,37 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
 
-# WebDriver
+# Initialize WebDriver
 service = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
+# options.add_argument('--headless')  # Uncomment for headless mode
 driver = webdriver.Chrome(service=service, options=options)
 
-url = 'https://reverb.com/marketplace?query=guitar&product_type=electric-guitars&condition=used'
-driver.get(url)
+# Base URLs for electric and acoustic guitars
+base_urls = {
+    'electric_guitars': 'https://reverb.com/marketplace?query=guitar&condition=used&country_of_origin=US&product_type=electric-guitars&category=solid-body&page=',
+    'acoustic_guitars': 'https://reverb.com/marketplace?query=guitar&condition=used&country_of_origin=US&product_type=acoustic-guitars&category=dreadnought&page='
+}
 
-time.sleep(10)  # wait 10 second
+# Define file path for saving HTML content
+file_path_prefix = 'C:\\Users\\DELL\\Desktop\\code\\Deeplearning\\Image-Based-Price-Estimation\\'
 
-# get html
-html = driver.page_source
+for guitar_type, base_url in base_urls.items():
+    for page in range(1, 41):  # Looping through pages 1 to 41
+        url = f"{base_url}{page}"
+        driver.get(url)
 
-# using BeautifulSoup format HTML
-soup = BeautifulSoup(html, 'html.parser')
-formatted_html = soup.prettify()
+        time.sleep(10)  # wait 10 second
 
-filepath = 'C:\\Users\\DELL\\Desktop\\code\\Deeplearning\\Image-Based-Price-Estimation\\page.html'
-# save.html file
-with open('page.html', 'w', encoding='utf-8') as file:
-    file.write(formatted_html)
+        # Retrieve and format the page HTML
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        formatted_html = soup.prettify()
 
-# quit WebDriver
+        # Construct the file name and path
+        filepath = f'{file_path_prefix}{guitar_type}_page_{page}.html'
+        with open(filepath, 'w', encoding='utf-8') as file:
+            file.write(formatted_html)
+
+# Quit the WebDriver
 driver.quit()
